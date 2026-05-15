e06f9f604742238c96b083855c3eded59eeba03a
ADITYA KUMAR <145112088+AdityawithA@users.noreply.github.com>
2026-05-15 22:41:37 +0530

    Add files via upload

---
+++ LengthofLastWord.java
@@ -0,0 +1,12 @@
+Q} Length of Last Word
+Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+A word is a maximal substring consisting of non-space characters only.
+
+
+class Solution {
+    public int lengthOfLastWord(String s) {
+        String[] words = s.trim().split(" ");
+        return words[words.length - 1].length();
+    }
+}
